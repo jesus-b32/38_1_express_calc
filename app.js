e518e4523cf975d8d31173756b1fd93544e40ed7
jesus <149453045+jesus-b32@users.noreply.github.com>
2024-06-10 20:16:02 -0700

    moved calculator function to a seperate file

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 const express = require('express');
 const ExpressError = require('./expressError');
+const {calcMode, calcMean, calcMedian, convertAndValidateNums } = require('./calc');
 
 const app = express();
 
 //average
 app.get('/mean', function(req, res, next) {
     try {
-        const nums = req.query;
-        if(Object.keys(nums).length === 0) throw new ExpressError(`Numbers are required`, 400);
-
-        const num_array = nums.nums.split(',').map(Number);
-        if(num_array.includes(NaN)) throw new ExpressError(`Please use only number values for calculator`, 400);
-        if(num_array.length === 0) throw new ExpressError(`Numbers are required`, 400);
-
-        let sum = 0;
-        for(let num of num_array) {
-            sum += num;
+        if (!req.query.nums) {
+            throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
         }
-        const mean = sum/num_array.length;
+        const nums = req.query.nums;
+        const num_array = convertAndValidateNums(nums);
+        if(num_array instanceof Error) {
+            throw new ExpressError(num_array.message);
+        }
     
         const mean_res = {response: {
             operation: 'mean',
-            value: mean
+            value: calcMean(num_array)
         }}
 
         return res.json(mean_res);
@@ -33,24 +30,18 @@ app.get('/mean', function(req, res, next) {
 //midpoint
 app.get('/median', (req, res, next) => {
     try {
-        const nums = req.query;
-        if(Object.keys(nums).length === 0) throw new ExpressError(`Numbers are required`, 400);
-
-        const num_array = nums.nums.split(',').map(Number);
-        if(num_array.includes(NaN)) throw new ExpressError(`Please use only number values for calculator`, 400);
-        if(num_array.length === 0) throw new ExpressError(`Numbers are required`, 400);
-
-        num_array.sort((a, b) => a - b);
-        let median;
-        if(num_array.length % 2 === 1) { //odd length array
-            median = num_array[Math.round(num_array.length / 2) - 1];
-        } else {
-            median = (num_array[(num_array.length / 2) - 1] + num_array[(num_array.length / 2)]) / 2;
+        if (!req.query.nums) {
+            throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+        }
+        const nums = req.query.nums;
+        const num_array = convertAndValidateNums(nums);
+        if(num_array instanceof Error) {
+            throw new ExpressError(num_array.message);
         }
 
         const median_res = {response: {
             operation: 'median',
-            value: median
+            value: calcMedian(num_array)
         }}
 
         return res.json(median_res);
@@ -62,32 +53,16 @@ app.get('/median', (req, res, next) => {
 // most frequent
 app.get('/mode', (req, res, next) => {
     try {
-        const nums = req.query;
-        if(Object.keys(nums).length === 0) throw new ExpressError(`Numbers are required`, 400);
-
-        const num_array = nums.nums.split(',').map(Number);
-        if(num_array.includes(NaN)) throw new ExpressError(`Please use only number values for calculator`, 400);
-        if(num_array.length === 0) throw new ExpressError(`Numbers are required`, 400);
-
-        // make an object with number in arraybeing keys and
-        //the number of occurances being the value
-        let modeObj = num_array.reduce((obj, num) => {
-            obj[num] = (obj[num] || 0) + 1;
-            return obj;
-        }, {});
-
-        const mode = []; //in an array in case there is more than one mode value
-        let occurance = 0;
-        for(const key of Object.keys(modeObj)) {
-            if (modeObj[key] > occurance) {
-                occurance = modeObj[key];
-                mode.length = 0;
-                mode.push(parseInt(key));
-            } else if (modeObj[key] === occurance) {
-                mode.push(parseInt(key));
-            }
+        if (!req.query.nums) {
+            throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+        }
+        const nums = req.query.nums;
+        const num_array = convertAndValidateNums(nums);
+        if(num_array instanceof Error) {
+            throw new ExpressError(num_array.message, 400);
         }
 
+        const mode = calcMode(num_array);
         let mode_res;
         if(mode.length === 1) {
             mode_res = {response: {
---
+++ calc.js
@@ -0,0 +1,80 @@
+/**
+ * FInd the mean of a number array
+ * @param {Array} num_array 
+ * @returns {float}
+ */
+function calcMean(num_array) {
+    let sum = 0;
+    for(let num of num_array) {
+        sum += num;
+    }
+    return sum/num_array.length;
+}
+
+/**
+ * Find the median of a number array
+ * @param {Array} num_array 
+ * @returns {float}
+ */
+function calcMedian(num_array) {
+    num_array.sort((a, b) => a - b);
+    let middleIndex = Math.floor(num_array.length / 2);
+
+    if(num_array.length % 2 === 1) { //odd length array
+        return num_array[middleIndex];
+    } else {
+        return (num_array[middleIndex] + num_array[middleIndex - 1]) / 2;
+    }
+}
+
+/**
+ * Find the mode of a number array
+ * @param {Array} num_array 
+ * @returns {float}
+ */
+function calcMode(num_array) {
+        // make an object with number in arraybeing keys and
+        //the number of occurances being the value
+        let modeObj = num_array.reduce((obj, num) => {
+            obj[num] = (obj[num] || 0) + 1;
+            return obj;
+        }, {});
+
+        const mode = []; //in an array in case there is more than one mode value
+        let occurance = 0;
+        for(const key of Object.keys(modeObj)) {
+            if (modeObj[key] > occurance) {
+                occurance = modeObj[key];
+                mode.length = 0;
+                mode.push(parseInt(key));
+            } else if (modeObj[key] === occurance) {
+                mode.push(parseInt(key));
+            }
+        }
+        return mode;
+}
+
+/**
+ * Attempt to convert a string of numbers to an array. Return an error if NaN is in array or array length is 0. Otherwise return the numArray
+ * @param {string} nums 
+ * @returns {Array|Error}
+ */
+function convertAndValidateNums (nums) {
+    const num_array = nums.split(',').map(Number);
+    if(num_array.includes(NaN)) {
+        return new Error(`Please use only number values for calculator`);
+    } 
+    if(num_array.length === 0) {
+        return new Error(`Numbers are required`);
+    }
+    return num_array;
+}
+
+
+
+module.exports = {
+    calcMean,
+    calcMedian,
+    calcMode,
+    convertAndValidateNums
+};
